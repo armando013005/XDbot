63a2b91a2b03dff17e168d76d05f5a1b4263290c
Your Name <you@example.com>
2020-10-28 15:10:59 -0700

    agregue el prefijo y una calc

--- XDbot.py
+++ XDbot.py
@@ -1,17 +1,24 @@
 import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='XD')
 client = discord.Client()
+
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def help(ctx, help):
+    await ctx.send("Hola, todavia no me asignan comandos")
 
-    if message.content.startswith('XDhelp'):
-        await message.channel.send('LO LOGRASTEEEEE')
+@bot.command()
+async def suma(ctx, a: int, b: int):
+    await ctx.send(a + b)
+
+    
+
 
 client.run('NzcwNDM0OTA2NTkyNjQxMDU1.X5dhaw.FoocCI0gbXBx_V0hEQ-4ZfSji84')
