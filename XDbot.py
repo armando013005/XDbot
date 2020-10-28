198ccd7288b0f9211342d1604f067050512ac470
Your Name <you@example.com>
2020-10-28 15:58:46 -0700

    add ban

--- XDbot.py
+++ XDbot.py
@@ -11,14 +11,21 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @bot.command()
-async def help(ctx, help):
+async def ayuda(ctx, ayuda):
     await ctx.send("Hola, todavia no me asignan comandos")
 
 @bot.command()
 async def suma(ctx, a: int, b: int):
     await ctx.send(a + b)
-
     
+import typing
 
+@bot.command()
+async def ban(ctx, members: commands.Greedy[discord.Member],
+                   delete_days: typing.Optional[int] = 0, *,
+                   reason: str):
+    """Mass bans members with an optional delete_days parameter"""
+    for member in members:
+        await member.ban(delete_message_days=delete_days, reason=reason)
 
 client.run('NzcwNDM0OTA2NTkyNjQxMDU1.X5dhaw.FoocCI0gbXBx_V0hEQ-4ZfSji84')
