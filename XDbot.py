b49b74f030e178a292d1bc42521b29746cc7d6f2
Your Name <you@example.com>
2020-10-28 16:26:33 -0700

    nose la neta

--- XDbot.py
+++ XDbot.py
@@ -3,29 +3,14 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='*')
 client = discord.Client()
+class MyClient(discord.client):
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
 
-
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@bot.command()
-async def test(ctx, *, arg):
-    await ctx.send(arg)
-
-@bot.command()
-async def suma(ctx, a: int, b: int):
-    await ctx.send(a + b)
-    
-import typing
-
-@bot.command()
-async def ban(ctx, members: commands.Greedy[discord.Member],
-                   delete_days: typing.Optional[int] = 0, *,
-                   reason: str):
-    """Mass bans members with an optional delete_days parameter"""
-    for member in members:
-        await member.ban(delete_message_days=delete_days, reason=reason)
+    @client.event
+    async def on_message(self, message):
+        if message.content.startswith("*Hello" or "*hola"):
+            await message.channel.send("Hi" or "hola")
 
 client.run('NzcwNDM0OTA2NTkyNjQxMDU1.X5dhaw.FoocCI0gbXBx_V0hEQ-4ZfSji84')
