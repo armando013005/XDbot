8b296faf8b71914c481ac3e1fe763477a56f0fe0
Your Name <you@example.com>
2020-10-29 14:11:43 -0700

    *info arreglado

--- XDbot.py
+++ XDbot.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
 import datetime
-import asyncio
+
+
 client = discord.Client()
 bot = commands.Bot(command_prefix='*', description="estoy en construccion :D")
+@bot.event
+async def on_ready():
+    game= discord.Game("*help | Agregame a tu sv fav :D")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+    print("ya")
 
 @bot.command()
 async def ping(ctx, description= "pong!!"):
@@ -13,24 +19,13 @@ async def ping(ctx, description= "pong!!"):
 async def suma(ctx,a: int, b: int):
     await ctx.send(a + b)
 
-@bot.event
-async def on_ready():
-    game= discord.Game("*help | Agregame a tu sv fav :D")
-    await bot.change_presence(status=discord.Status.online, activity=game)
-    print("ya")
-
-
 @bot.command()
 async def info(ctx):
-    await ctx.send(embed = discord.Embed(
-     title=f"{ctx.guild.name}",
-     description="Info of the this server", 
-     timestamp=datetime.datetime.utcnow(), 
-     color=discord.Color.blue(),
-     nameOwner = "Server Owner",ValueOwner=f"{ctx.guild.owner}",
-     nameRegion="Server Region", ValueRegion =f"{ctx.guild.region}",
-     nameServerID="Server ID", ValueServerID=f"{ctx.guild.id}")
-    )
-     
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Informacion del servidor actual :D", timestamp=datetime.datetime.utcnow(), color=discord.Color.gold())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    await ctx.send(embed=embed)
 
 bot.run('NzcwNDM0OTA2NTkyNjQxMDU1.X5dhaw.FoocCI0gbXBx_V0hEQ-4ZfSji84')
