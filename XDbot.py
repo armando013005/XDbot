6d6045f3c8c8a65b190699b18f245183576a3b39
Your Name <you@example.com>
2020-10-30 13:36:49 -0700

    act help

--- XDbot.py
+++ XDbot.py
@@ -1,15 +1,35 @@
 import discord
 from discord.ext import commands
 import datetime
-
+import discord.activity
 
 client = discord.Client()
-bot = commands.Bot(command_prefix='*', description="estoy en construccion :D")
+bot = commands.Bot(command_prefix='XD', description="estoy en construccion :D")
 @bot.event
 async def on_ready():
-    game= discord.Game("*help | Agregame a tu sv fav :D |")
+    game= discord.Game("XDHelp | Agregame a tu sv fav :D |")
     await bot.change_presence(status=discord.Status.online, activity=game)
     print("ya")
+
+@bot.command()
+async def Help(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Info del comando ayuda :D", timestamp=datetime.datetime.utcnow(), color=discord.Color.gold())
+    embed.add_field(name="XDHelp", value="Da este comando")
+    embed.add_field(name="XDpong", value="Dice ping¡¡")
+    embed.add_field(name="XDjoin", value="Dice cuando se unio el usuario")
+    embed.add_field(name="XDsuma", value="Es una calculadora de suma xd")
+    embed.add_field(name="XDinfo", value="Da la informacion del servidor actual")
+    embed.add_field(name="XDQueHace", value="Da la actividad del usuario (en construccion)")
+    await ctx.send(embed=embed)
+    pass
+
+@bot.command()
+async def QueHace(ctx, *, member: discord.Member, BaseActivity: discord.Activity):
+    await ctx.send('{0} esta jugando {0.BaseActivity}'.format(member))
+
+@bot.command()
+async def join(ctx, *, member: discord.Member):
+    await ctx.send('@{0} se unio el {0.joined_at} al servidor 'f"{ctx.guild.name}".format(member))
 
 @bot.command()
 async def ping(ctx, description= "pong!!"):
